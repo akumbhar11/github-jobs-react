0e3c1136afae413bfcfd15ece6e39b421b366f3c
akumbhar <aditya.kumbhar.1996@gmail.com>
2020-08-02 16:49:28 +0530

    Updates

---
+++ precache-manifest.00674482dd7da3ec91dd763733ce00cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4fbac4fa5eaf48fd03badaa9f679be",
+    "url": "//akumbhar11.github.io/github-jobs-react/index.html"
+  },
+  {
+    "revision": "788fe9b94ebdf0ac7ec7",
+    "url": "//akumbhar11.github.io/github-jobs-react/static/css/main.f08e5790.chunk.css"
+  },
+  {
+    "revision": "b7c75a72425d547faea1",
+    "url": "//akumbhar11.github.io/github-jobs-react/static/js/2.9a00f951.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "//akumbhar11.github.io/github-jobs-react/static/js/2.9a00f951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788fe9b94ebdf0ac7ec7",
+    "url": "//akumbhar11.github.io/github-jobs-react/static/js/main.aae41569.chunk.js"
+  },
+  {
+    "revision": "8cdc0c7011f37949a39b",
+    "url": "//akumbhar11.github.io/github-jobs-react/static/js/runtime-main.0a72b789.js"
+  }
+]);
